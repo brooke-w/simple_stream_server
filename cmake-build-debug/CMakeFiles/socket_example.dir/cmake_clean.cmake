c725f2dce675cb3eb14e86a43503c8c7e68c003d
Brooke Weborg <brookeweborg@yahoo.com>
2017-11-03 14:08:38 -0400

    Made Project in CLion C++14

---
+++ cmake-build-debug/CMakeFiles/socket_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socket_example.dir/main.cpp.o"
+  "socket_example.pdb"
+  "socket_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/socket_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
