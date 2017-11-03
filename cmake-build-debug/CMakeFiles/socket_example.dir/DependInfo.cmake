afdc5f2fefdd307dbd9e3412dd3e7e11f558f365
Brooke Weborg <brookeweborg@yahoo.com>
2017-11-03 14:30:02 -0400

    no message

--- cmake-build-debug/CMakeFiles/socket_example.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/socket_example.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/brookeweborg/socket_example/main.cpp" "/Users/brookeweborg/socket_example/cmake-build-debug/CMakeFiles/socket_example.dir/main.cpp.o"
+  "/Users/brookeweborg/simple_stream_server/main.cpp" "/Users/brookeweborg/simple_stream_server/cmake-build-debug/CMakeFiles/socket_example.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
